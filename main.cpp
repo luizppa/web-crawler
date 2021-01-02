77315aaf3cbcc2aeab5e7db7c5eea4290dd28f35
Luiz Philippe <luizphilippep@gmail.com>
2021-01-01 21:28:58 -0300

    Breadth-first-like crawling (single thread and multi thread)

--- main.cpp
+++ main.cpp
@@ -1,46 +1,130 @@
 #include <CkSpider.h>
 #include<iostream>
+#include<fstream>
 #include<chrono>
+#include<vector>
+#include<queue>
+#include<thread>
 
-void crawl_through(char const starting_url[], int n){
-    CkSpider spider;
-    int pages_to_crawl = n+1;
-    int total_time = 0;
-
-    spider.Initialize(starting_url);
-    spider.AddUnspidered(starting_url);
-
-    for (int i = 0; i < pages_to_crawl; i++) {
-        bool success;
-        auto start_time = std::chrono::high_resolution_clock::now();
-        success = spider.CrawlNext();
-        auto end_time = std::chrono::high_resolution_clock::now();
-        total_time += std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-        if(success) {
-            std::cout << spider.lastHtmlTitle()  << std::endl << spider.lastUrl() << std::endl << std::endl;
+std::vector<std::string> read_input(const char* input_file_path){
+    std::vector<std::string> seed;
+    std::ifstream input_file;
+    input_file.open(input_file_path);
+    if(input_file.is_open()){
+        std::string url;
+        while(!input_file.eof()){
+            input_file  >> url;
+            seed.push_back(url);
         }
-        else {
-            if (spider.get_NumUnspidered() == 0) {
-                std::cout << "No more URLs to spider" << std::endl;
-                break;
-            }
-            else {
-                std::cout << spider.lastErrorText() << std::endl;
-            }
+    }
+    return seed;
+}
+
+void crawl_async(CkSpider* spider, int* total_time, int* visited_pages){
+    bool success;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    success = spider->CrawlNext();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    if(success) {
+        *visited_pages += 1;
+        *total_time += std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+        std::cout << spider->lastHtmlTitle()  << std::endl << spider->lastUrl() << std::endl << std::endl;
+    }
+    else {
+        if(spider->get_NumUnspidered() == 0){
+            std::cout << "FIM" << std::endl;
+        }
+        std::cout << spider->lastErrorText() << std::endl;
+    }
+}
+
+void crawl_through_async(std::vector<std::string> seed_urls){
+    int total_time = 0;
+    int visited_pages = 0;
+    std::queue<std::string> long_term_scheduler;
+
+    for(std::vector<std::string>::iterator it = seed_urls.begin(); it != seed_urls.end(); ++it){
+        long_term_scheduler.push(*it);
+    }
+
+    while(visited_pages < 100000 && long_term_scheduler.size() > 0){
+        const char* current_url = long_term_scheduler.front().c_str();
+        CkSpider spider;
+        spider.Initialize(current_url);
+        spider.AddUnspidered(current_url);
+        long_term_scheduler.pop();
+
+        while(spider.get_NumUnspidered() != 0 && visited_pages < 100000) {
+            std::thread crawl(crawl_async, &spider, &total_time, &visited_pages);
+            crawl.detach();
+            spider.SleepMs(100);
+        }
+        std::cout << "Outbounds " << spider.get_NumOutboundLinks() << std::endl;
+
+        for(int i = 0; i < spider.get_NumOutboundLinks(); i++){
+            long_term_scheduler.push(std::string(spider.getOutboundLink(i)));
         }
     }
 
     float total_time_in_seconds = ((float)total_time)/1000.0;
-    float average_time_in_seconds = total_time_in_seconds/((float)pages_to_crawl);
+    float average_time_in_seconds = total_time_in_seconds/((float)visited_pages);
+    std::cout << "Duration: " << total_time_in_seconds << " seconds" << std::endl;
+    std::cout << "Average time spent per page: " << average_time_in_seconds << " seconds" << std::endl;
+}
+
+void crawl_through(std::vector<std::string> seed_urls){
+    int total_time = 0;
+    int visited_pages = 0;
+    std::queue<std::string> long_term_scheduler;
+
+    for(std::vector<std::string>::iterator it = seed_urls.begin(); it != seed_urls.end(); ++it){
+        long_term_scheduler.push(*it);
+    }
+
+    while(visited_pages < 100000 && long_term_scheduler.size() > 0){
+        const char* current_url = long_term_scheduler.front().c_str();
+        CkSpider spider;
+        spider.Initialize(current_url);
+        spider.AddUnspidered(current_url);
+        long_term_scheduler.pop();
+
+        while(visited_pages < 100000 && spider.get_NumUnspidered() != 0) {
+            bool success;
+            auto start_time = std::chrono::high_resolution_clock::now();
+            success = spider.CrawlNext();
+            auto end_time = std::chrono::high_resolution_clock::now();
+            if(success) {
+                visited_pages++;
+                total_time += std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+                std::cout << spider.lastHtmlTitle()  << std::endl << spider.lastUrl() << std::endl << std::endl;
+            }
+            else {
+                if(spider.get_NumUnspidered() == 0){
+                    std::cout << "FIM" << std::endl;
+                }
+                std::cout << spider.lastErrorText() << std::endl;
+            }
+            spider.SleepMs(100);
+        }
+        std::cout << "Outbounds " << spider.get_NumOutboundLinks() << std::endl;
+
+        for(int i = 0; i < spider.get_NumOutboundLinks(); i++){
+            long_term_scheduler.push(std::string(spider.getOutboundLink(i)));
+        }
+    }
+
+    float total_time_in_seconds = ((float)total_time)/1000.0;
+    float average_time_in_seconds = total_time_in_seconds/((float)visited_pages);
     std::cout << "Duration: " << total_time_in_seconds << " seconds" << std::endl;
     std::cout << "Average time spent per page: " << average_time_in_seconds << " seconds" << std::endl;
 }
 
 int main(int argc, char* const argv[]){
-    if(argc < 3){
+    if(argc < 2){
         std::cout << "Not enough arguments" << std::endl;
         return 1;
     }
-    crawl_through(argv[1], atoi(argv[2]));
+    std::vector<std::string> seed_urls =  read_input(argv[1]);
+    crawl_through(seed_urls);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC=g++
 STD=c++17
-CFLAGS=-Wall -Wextra -std=$(STD)
+CFLAGS=-Wall -Wextra -std=$(STD) -pthread
 CLIBS=-l chilkat-9.5.0
 TARGET_NAME=ir_tp1
 TARGET_PATH=$(BUILD_PATH)/$(TARGET_NAME)
@@ -15,7 +15,7 @@ clean:
 	rm -rf $(BUILD_PATH)/*
 
 run:
-	$(TARGET_PATH) https://ufmg.br/ 10
+	$(TARGET_PATH) ./input/seed > ./output/crawler.log
 
 mem:
-	valgrind --leak-check=full --show-leak-kinds=all $(TARGET_PATH) https://ufmg.br/ 10 > ./output/mem.log
+	valgrind $(TARGET_PATH) ./input/seed > ./output/mem.log
